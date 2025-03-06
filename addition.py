9e28ba7e5bec3061b167a7698191799c626f926d
sudha9159968236 <sudha.tamtree@gmail.com>
2025-03-06 15:35:56 +0530

    addition and subtraction

--- addition.py
+++ addition.py
@@ -10,7 +10,19 @@
 # print(y)
 
 # multiple variable
-x,y,z="orange","banana","cherry"
-print(x)
-print(y)
-print(z)
+# x,y,z="orange","banana","cherry"
+# print(x)
+# print(y)
+# print(z)
+
+# add two values
+# a=10
+# b=45
+# c=a+b
+# print(c)
+
+# subtract
+a=67
+b=74
+c=a-b
+print(c)
